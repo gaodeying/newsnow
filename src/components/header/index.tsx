906669a5e6e283045f74bfc4dd57514d501550be
搞得赢 <gaodeying@meituan.com>
2025-04-17 18:08:11 +0800

    更新titile布局

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -45,7 +45,7 @@ export function Header() {
       <span className="flex justify-self-start">
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
-          <span className="text-2xl font-brand line-height-none!">
+          <span className="text-2xl font-brand flex items-center">
             <p>东厂</p>
             <p className="mt--1">
               <span className="color-primary-6">情</span>
